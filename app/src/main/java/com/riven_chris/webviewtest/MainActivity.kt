a0439b4b1f55882ee1c12ec229165ae9e53f54c7
riven_chris <hsj7107713@163.com>
2020-06-16 00:59:23 +0800

    commit

--- app/src/main/java/com/riven_chris/webviewtest/MainActivity.kt
+++ app/src/main/java/com/riven_chris/webviewtest/MainActivity.kt
@@ -1,22 +1,123 @@
-import androidx.appcompat.app.AppCompatActivity
+package com.riven_chris.webviewtest
+
+import android.net.http.SslError
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.webkit.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.riven_chris.webviewtest.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lifecycleScope.launchWhenResumed {
-//            val result = withContext(Dispatchers.IO) {
-//                (URL("https://cms.51xuetang.com/m/article/getArticle?articleId=1272166871333892097").readText())
-//            }
-//            val jsonObject = JSONObject(result)
-//            val data = jsonObject.getJSONObject("data")
-//            val htmlData = data.getString("content")
-//            webview.loadData(htmlData, "text/html", "UTF-8")
+
+        btnLoad.setOnClickListener {
+            loadHtml()
+        }
+
+        val settings = webview.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING);
+        } else {
+            settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+        }
+
+
+        webview.apply {
+            setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+            webChromeClient = WebChromeClient()
+            webViewClient = object : WebViewClient() {
+                override fun onReceivedError(
+                    view: WebView?,
+                    errorCode: Int,
+                    description: String?,
+                    failingUrl: String?
+                ) {
+                    super.onReceivedError(view, errorCode, description, failingUrl)
+                }
+
+                override fun onReceivedError(
+                    view: WebView?,
+                    request: WebResourceRequest?,
+                    error: WebResourceError?
+                ) {
+                    super.onReceivedError(view, request, error)
+                }
+
+                override fun onReceivedHttpError(
+                    view: WebView?,
+                    request: WebResourceRequest?,
+                    errorResponse: WebResourceResponse?
+                ) {
+                    super.onReceivedHttpError(view, request, errorResponse)
+                }
+
+                override fun onReceivedSslError(
+                    view: WebView?,
+                    handler: SslErrorHandler?,
+                    error: SslError?
+                ) {
+                    super.onReceivedSslError(view, handler, error)
+                }
+
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                    return super.shouldOverrideUrlLoading(view, url)
+                }
+
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    return super.shouldOverrideUrlLoading(view, request)
+                }
+            }
+
+            settings.apply {
+                javaScriptEnabled = true
+                useWideViewPort = false
+                allowFileAccess = true
+                allowContentAccess = true
+                blockNetworkImage = false
+                blockNetworkLoads = false
+                domStorageEnabled = true
+                setAppCacheEnabled(true)
+                setDefaultTextEncodingName("UTF-8")
+            }
         }
     }
+
+    fun loadHtml() {
+
+        lifecycleScope.launch {
+            val result = withContext(Dispatchers.IO) {
+//                val id = "1272464926989930497"
+                val id = "1272524908989042690"
+                (URL("https://cms.51xuetang.com/m/article/getArticle?articleId=$id").readText())
+            }
+            val jsonObject = JSONObject(result)
+            val data = jsonObject.getJSONObject("data")
+            var htmlData = data.getString("content")
+            webview.loadData(htmlData, "text/html;charset=utf-8", "UTF-8")
+        }
+    }
+
+    private fun getHtmlData(bodyHTML: String): String {
+        val head =
+            "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\"> <style>html{padding:15px;} body{word-wrap:break-word;font-size:13px;padding:0px;margin:0px} p{padding:0px;margin:0px;font-size:13px;color:#222222;line-height:1.3;} img{padding:0px,margin:0px;max-width:100%; width:auto; height:auto;}</style></head>";
+        return "<html>" + head + "<body>" + bodyHTML + "</body></html>"
+    }
+
 }
